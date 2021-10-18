7d252246f0708f1a515f14c10b29d1ac2ee033c9
titkul <92681557+titkul@users.noreply.github.com>
2021-10-18 19:33:20 +0700

    Push code next

---
+++ hieu.py
@@ -0,0 +1,11 @@
+you = "hello"
+
+if you == "":
+    robot_brain = "I can't hear you, try again"
+elif you == "hello":
+    robot_brain = "Hello You"
+elif you == "today":  
+    robot_brain = "thu 6"      
+else:         
+    robot_brain = "I'm fine thank you and you"
+print(robot_brain)    
