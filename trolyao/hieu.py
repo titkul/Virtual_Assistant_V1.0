e5fa5fb706978208bb314555528244e9765b66cd
titkul <92681557+titkul@users.noreply.github.com>
2021-10-18 19:29:12 +0700

    Push code to git

---
+++ trolyao/hieu.py
@@ -0,0 +1,11 @@
+you = "hello"
+
+if you == "":
+        robot_brain = "I can't hear you, try again"
+elif you == "hello":
+    robot_brain = "Hi You!"
+elif you == "today":
+    robot_brain = "thu 5"
+elif:
+    robot_brain = "I'm file thank you and you"
+print(robot_brain)
